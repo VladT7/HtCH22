b44fc5351aadd2b7837d6176e9ce9e824d0d3499
Abhinav Thota <abhinavthota01@gmail.com>
2022-11-19 17:27:47 -0800

    Create readJson.py
    returns the name of product from UPC code

---
+++ readJson.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+
+# import json
+import json
+# store the URL in url as
+# parameter for urlopen
+upc = input("Enter UPC:")
+url = "https://world.openfoodfacts.org/api/v0/product/{upcToPass}.json".format(
+    upcToPass=upc)
+
+# store the response of URL
+response = urlopen(url)
+
+# storing the JSON response
+# from url in data
+data_json = json.loads(response.read())
+
+# print the name of product
+print(data_json['product']['_keywords'])
